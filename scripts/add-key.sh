8b2f870e0d600216d27f8d603e0a4e145a7d6f3c
Sebastian <sebastian.ovide@gmail.com>
2019-01-05 01:19:29 +0000

    4th tentative of building ios.

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -16,7 +16,7 @@ security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 # Add certificates to keychain and allow codesign to access them
 security import ./cordova-app/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./cordova-app/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./cordova-app/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $APPLE_KEY_PASSWORD -T /usr/bin/codesign
+security import ./cordova-app/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 
 # Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
