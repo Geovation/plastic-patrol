cb8cb6dd0882b375d86f0fe6c6bdd71964f22f75
Sebastian <sebastian.ovide@gmail.com>
2019-01-05 01:50:50 +0000

    5th tentative of building ios.

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -18,6 +18,9 @@ security import ./cordova-app/apple.cer -k ~/Library/Keychains/ios-build.keychai
 security import ./cordova-app/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./cordova-app/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 
+# see https://docs.travis-ci.com/user/common-build-problems/
+security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
+
 # Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp "./cordova-app/Geovation.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
