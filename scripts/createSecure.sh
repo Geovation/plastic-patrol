ef80658406cdc3151d3923210c23277f6d4b7a55
Sebastian <sebastian.ovide@gmail.com>
2019-01-10 15:17:07 +0000

    deploy android beta in travis 5

--- scripts/createSecure.sh
+++ scripts/createSecure.sh
@@ -2,7 +2,7 @@
 
 pushd cordova-app
 
-tar cvf secrets.tar build.json *.cer *.p12 *.mobileprovision *.keystore serviceAccountKey-plastic-patrol.json
+tar cvf secrets.tar build.json *.cer *.p12 *.mobileprovision *.keystore serviceAccountKey-plastic-patrol.json GooglePlayAndroidDeveloper.json
 travis encrypt-file secrets.tar secrets.tar.enc -p -r Geovation/plastic-patrol
 
 popd
