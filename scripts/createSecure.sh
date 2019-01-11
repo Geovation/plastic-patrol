7e1dab0326ec8fc461ebd2e69c7292629f8a67a2
Sebastian Ovide <sebastian.ovide@gmail.com>
2019-01-11 15:58:41 +0000

    added ios pakcage descriptions (#107)

--- scripts/createSecure.sh
+++ scripts/createSecure.sh
@@ -5,4 +5,7 @@ pushd cordova-app
 tar cvf secrets.tar build.json *.cer *.p12 *.mobileprovision *.keystore serviceAccountKey-plastic-patrol.json GooglePlayAndroidDeveloper.json
 travis encrypt-file secrets.tar secrets.tar.enc -p -r Geovation/plastic-patrol
 
+echo you should run this command
+echo "cp secrets.tar ../secrets/plastic-patrol.tar"
+
 popd
