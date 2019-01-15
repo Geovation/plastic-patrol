3d65244edb640b8f47ad73fa09e962c8ff4ca631
Sebastian Ovide <sebastian.ovide@gmail.com>
2019-01-15 09:34:42 +0000

    using it-ci@geovation.uk account (#116)

--- scripts/runWithVars.sh
+++ scripts/runWithVars.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x;
+
 VERSION_MAYOR=$(echo $npm_package_version| cut -d'.' -f 1)
 VERSION_MINOR=$(echo $npm_package_version| cut -d'.' -f 2)
 
@@ -7,6 +9,5 @@ export REACT_APP_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER:-"0"}
 export REACT_APP_VERSION="$VERSION_MAYOR.$VERSION_MINOR.$REACT_APP_BUILD_NUMBER"
 export REACT_APP_TITLE=$npm_package_title
 
-set -x;
 eval $@
 set +x;
