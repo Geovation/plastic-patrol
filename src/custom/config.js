3ed7cce61ccd52002553fdbb96204bf4f6a908ed
Sebastian Ovide <sebastian.ovide@gmail.com>
2019-05-28 11:23:41 +0100

    API full UR (#181)

--- src/custom/config.js
+++ src/custom/config.js
@@ -212,7 +212,8 @@ export default {
     UPLOAD_REQUIRES_LOGIN: true
   },
   API: {
-    URL: "https://app.plasticpatrol.co.uk/api",
+    // URL: "https://api.plasticpatrol.co.uk",
+    URL: "https://us-central1-plastic-patrol-fd3b3.cloudfunctions.net/api",
     // URL: "http://localhost:5000/plastic-patrol-fd3b3/us-central1/api"
   },
   LEADERBOARD_FIELD: {
