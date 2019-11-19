5e35942b1e4b52e067058139d65607d6cf85beb8
Sebastian Ovide <sebastian.ovide@geovation.uk>
2019-11-19 08:44:54 +0000

    remove old backups (#276)
    * deleted old ones
    
    * node 13

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -17,5 +17,9 @@ gsutil -m rsync -r bks/users gs://plastic-patrol-bks/$FOLDER/users
 # save storage
 gsutil -m rsync -r gs://plastic-patrol-fd3b3.appspot.com gs://plastic-patrol-bks/$FOLDER/storage
 
-# delete older than 1 month
-# TODO
+# delete old one. Leave the newyer 6
+TO_DELETE=`(gsutil ls gs://plastic-patrol-bks/ | awk 'n>=6 { print a[n%6] } { a[n%6]=$0; n=n+1 }')`
+echo $TO_DELETE | xargs gsutil -m rm -r
+# echo $TO_DELETE | gsutil -m rm -r -I
+
+gsutil ls gs://plastic-patrol-bks/
